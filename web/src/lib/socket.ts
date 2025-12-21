bf1fd6705dea92d89f941500d8bd784cf55bfe95
Eduardo <dunabozny@gmail.com>
2025-12-21 12:01:44 -0300

    Correção pacotes

--- web/src/lib/socket.ts
+++ web/src/lib/socket.ts
@@ -4,7 +4,7 @@ import { ClientToServerEvents, ServerToClientEvents } from "../interfaces/socket
 type TypedSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
 
 export function getSocket(): TypedSocket {
-    return io(process.env.NEXT_PUBLIC_URL_SOCKET, {
+    return io(globalThis.window === undefined ? process.env.INTERNAL_URL_API_SOCKET : process.env.NEXT_PUBLIC_URL_SOCKET, {
         withCredentials: true,
         autoConnect: false,
     });
--- web/src/requests/axios.config.ts
+++ web/src/requests/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_URL_API,
+    baseURL: globalThis.window === undefined ? process.env.INTERNAL_URL_API : process.env.NEXT_PUBLIC_URL_API,
     withCredentials: true,
 });
 
