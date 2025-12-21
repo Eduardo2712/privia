a0cfaa600ebebac3e83c6a9bf46cf45794725a83
Eduardo <dunabozny@gmail.com>
2025-12-21 13:36:52 -0300

    Correção pacotes

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    // Optimize Docker runtime by emitting a standalone server
+    output: "standalone",
 };
 
 export default nextConfig;
