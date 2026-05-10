c3f1f6ed0e322c0dbafd1fb8660418218b525f5c
Eduardo <dunabozny@gmail.com>
2026-05-09 21:13:48 -0300

    Teste

--- web/src/requests/axios.config.ts
+++ web/src/requests/axios.config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    withCredentials: true,
     baseURL: process.env.NEXT_PUBLIC_URL_API,
+    withCredentials: true,
 });
 
 export default axiosInstance;
